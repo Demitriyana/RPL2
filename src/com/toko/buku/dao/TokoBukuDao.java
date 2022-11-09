fd8b6edc136e34565ba031bfd80dbca4b9ff5863
Demitriyana <demitriyana93@gmail.com>
2022-11-09 07:58:56 +0700

    first commit

---
+++ src/com/toko/buku/dao/TokoBukuDao.java
@@ -0,0 +1,13 @@
+package com.toko.buku.dao;
+
+import com.toko.buku.model.TokoBuku;
+import java.util.List;
+
+
+public interface TokoBukuDao {
+    public void save(TokoBuku tokobuku);
+    public void update(TokoBuku tokobuku);
+    public void delete(TokoBuku tokobuku);
+    public TokoBuku get(String kd_buku);
+    public List<TokoBuku> getList();
+}
